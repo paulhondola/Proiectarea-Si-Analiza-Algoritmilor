d21ae15f53e89b07e2bc3af8b5af8e68025f026f
Paul Hondola <paulhondola@gmail.com>
2025-03-17 16:56:41 +0200

    added binary tree implementation for lab3

---
+++ Lab/lab3_binary_tree_implementation/lab3.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdint.h>
+#include "Bib.h"
+#include <stdlib.h>
+
+TTree bulkInsert(TTree a, FILE *f) {
+	
+	int x = 0, y = 0;
+
+    while (fscanf(f, "%d %d", &y, &x) == 2) { // Ensures two values are read
+        printf("Inserting %d %d\n", x, y);
+        a = InsertNode(a, x, y);  // Ensure InsertNode correctly returns the new tree structure
+    }
+
+	return a;
+}
+
+int main(int argc, char** argv) {
+
+	if (argc != 2) {
+		printf("Usage: %s <input_file>\n", argv[0]);
+		return 1;
+	}
+
+	FILE* f = fopen(argv[1], "r");
+	if (f == NULL) {
+		printf("Could not open file %s\n", argv[1]);
+		return 2;
+	}
+
+	TTree a = { 0 };
+	a = InitTree(a);
+	a = bulkInsert(a, f);
+
+	TTree bst = convertFCRStoBST(a);
+	inOrder(a);
+	printf("\n");
+	inOrder(bst);
+	
+	fclose(f);
+
+	return 0;
+}

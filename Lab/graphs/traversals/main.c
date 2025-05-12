d38b136312fa59e34ea4cfabd3005a8849ff32fc
Paul Hondola <paulhondola@gmail.com>
2025-05-13 00:38:35 +0300

    finished graph traversal exercises

--- Lab/graphs/traversals/main.c
+++ Lab/graphs/traversals/main.c
@@ -36,50 +36,95 @@ void read_matrix_data(int matrix[][MAX_MATRIX_SIZE], int *size, FILE *f) {
 }
 
 void print_matrix(int matrix[][MAX_MATRIX_SIZE], int size) {
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
+  for (int i = 1; i <= size; i++) {
+    for (int j = 1; j <= size; j++) {
       printf("%d ", matrix[i][j]);
     }
     printf("\n");
   }
 }
 
-void stack_test() {
-  stack stack;
+void bfs(int matrix[][MAX_MATRIX_SIZE], int size, int start) {
 
-  init_stack(&stack);
+  int visited[MAX_MATRIX_SIZE] = {0};
+  queue q;
+  init_queue(&q);
 
-  for (int i = 0; i < 10; i++) {
-    push(&stack, i);
+  // enqueue the starting node
+  enqueue(&q, start);
+  visited[start] = 1;
+
+  while (!is_empty_queue(&q)) {
+    int current = dequeue(&q);
+    printf("%d ", current);
+
+    // visit all the neighbors of the current node
+    for (int i = 1; i <= size; i++) {
+      if (matrix[current][i] && !visited[i]) {
+        enqueue(&q, i);
+        visited[i] = 1;
+      }
+    }
   }
-
-  print_stack(&stack);
-
-  for (int i = 0; i < 5; i++) {
-    pop(&stack);
-  }
-
-  print_stack(&stack);
-
-  printf("\n");
 }
 
-void queue_test() {
-  queue queue;
+void dfs(int matrix[][MAX_MATRIX_SIZE], int size, int start) {
+  stack s;
+  init_stack(&s);
 
-  init_queue(&queue);
+  int visited[MAX_MATRIX_SIZE] = {0};
 
-  for (int i = 0; i < 10; i++) {
-    enqueue(&queue, i);
+  // push the starting node
+  push(&s, start);
+  visited[start] = 1;
+
+  while (!is_empty_stack(&s)) {
+    int current = pop(&s);
+    printf("%d ", current);
+
+    // visit all the neighbors of the current node
+    for (int i = size; i >= 1; i--) {
+      if (matrix[current][i] && !visited[i]) {
+        push(&s, i);
+        visited[i] = 1;
+      }
+    }
+  }
+}
+
+int shortest_path(int matrix[][MAX_MATRIX_SIZE], int size, int start, int end) {
+
+  int visited[MAX_MATRIX_SIZE] = {0};
+  int path[MAX_MATRIX_SIZE] = {0};
+
+  stack s;
+  init_stack(&s);
+
+  // push the starting node
+  push(&s, start);
+  visited[start] = 1;
+
+  while (!is_empty_stack(&s)) {
+    int current = pop(&s);
+    // printf("%d ", current);
+
+    // visit all the neighbors of the current node
+    for (int i = size; i >= 1; i--) {
+      if (matrix[current][i] && !visited[i]) {
+        push(&s, i);
+        visited[i] = 1;
+        path[i] = path[current] + 1;
+      }
+    }
   }
 
-  print_queue(&queue);
-
-  for (int i = 0; i < 5; i++) {
-    dequeue(&queue);
+  // reconstruct the path from start to end
+  if (!visited[end]) {
+    printf("No path found\n");
+    return -1;
   }
 
-  print_queue(&queue);
+  return path[end];
 }
 
 int main(void) {
@@ -92,7 +137,7 @@ int main(void) {
 
   int size = 0;
   int matrix[MAX_MATRIX_SIZE][MAX_MATRIX_SIZE];
-  memset(matrix, 0, MAX_MATRIX_SIZE * MAX_MATRIX_SIZE);
+  memset(matrix, 0, sizeof(matrix));
 
   read_matrix_data(matrix, &size, f);
   fclose(f);
@@ -101,8 +146,18 @@ int main(void) {
 
   printf("\n");
 
-  stack_test();
-  queue_test();
+  // BFS starting from node 1
+  printf("BFS starting from node 1:\n");
+  bfs(matrix, size, 1);
+
+  printf("\n");
+  // DFS starting from node 1
+  printf("DFS starting from node 1:\n");
+  dfs(matrix, size, 1);
+
+  // Shortest path from node 1 to node 5
+  printf("\nShortest path from node 1 to node 5:\n");
+  printf("%d\n", shortest_path(matrix, size, 1, 5));
 
   return 0;
 }
